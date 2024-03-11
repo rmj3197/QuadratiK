3785e2bfc25d76f2aa793e0d8bbde18f3884d57b
Raktim Mukhopadhyay <rmj.3197@gmail.com>
2024-03-11 15:10:00 -0400

    Update test_uniformity_test.py

--- tests/test_uniformity_test.py
+++ tests/test_uniformity_test.py
@@ -18,7 +18,10 @@ class TestUniformityTest(unittest.TestCase):
         self.assertIsInstance(uniformity_test.u_statistic_un_, (int, float))
         self.assertIsInstance(uniformity_test.u_statistic_cv_ ,(int, float))
         self.assertIsInstance(uniformity_test.u_statistic_h0_, np.bool_)
+        
         self.assertIsInstance(uniformity_test.v_statistic_vn_, (int, float))
+        self.assertIsInstance(uniformity_test.v_statistic_cv_ ,(int, float))
+        self.assertIsInstance(uniformity_test.v_statistic_h0_, np.bool_)
 
         self.assertIsInstance(uniformity_test.stats(), pd.DataFrame)
         self.assertIsInstance(uniformity_test.summary(print_fmt="html"), str)
@@ -44,6 +47,24 @@ class TestUniformityTest(unittest.TestCase):
             rho=0.8, random_state=None, num_iter=10, n_jobs=4
         ).test(x_sp)
         self.assertIsInstance(uniformity_test.u_statistic_un_, (int, float))
-        self.assertIsInstance(uniformity_test.v_statistic_vn_, (int, float))
         self.assertIsInstance(uniformity_test.u_statistic_cv_ ,(int, float))
+        self.assertIsInstance(uniformity_test.u_statistic_h0_, np.bool_)
+        
+        self.assertIsInstance(uniformity_test.v_statistic_vn_, (int, float))
+        self.assertIsInstance(uniformity_test.v_statistic_cv_ ,(int, float))
         self.assertIsInstance(uniformity_test.v_statistic_h0_, np.bool_)
+    
+    def test_uniformity_rndm_state_dataframe_dim1(self):
+        x_sp = pd.DataFrame(sample_hypersphere(npoints=100, ndim=3))
+        uniformity_test = PoissonKernelTest(
+            rho=0.8, random_state=42, num_iter=10, n_jobs=4
+        ).test(x_sp)
+        self.assertIsInstance(uniformity_test.u_statistic_un_, (int, float))
+        self.assertIsInstance(uniformity_test.u_statistic_cv_ ,(int, float))
+        self.assertIsInstance(uniformity_test.u_statistic_h0_, np.bool_)
+        
+        self.assertIsInstance(uniformity_test.v_statistic_vn_, (int, float))
+        self.assertIsInstance(uniformity_test.v_statistic_cv_ ,(int, float))
+        self.assertIsInstance(uniformity_test.v_statistic_h0_, np.bool_)
+    
+    
